1ca2ae13aa2228e556d2797001f725d6bb2eda7b
Dan Sadaka <dan@web-site1.com>
2024-10-03 10:09:12 -0400

    tweaked responses_controller.rb

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -26,7 +26,7 @@ class ResponsesController < ApplicationController
     respond_to do |format|
       if @response.save
         format.html { redirect_to question_url, notice: "Response was successfully created." }
-        format.turbo_stream { render :update, notice: "Response was successfully created." }
+        format.turbo_stream { render :update }
         format.json { render :show, status: :created, location: @response }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +40,7 @@ class ResponsesController < ApplicationController
   def update
     respond_to do |format|
       if @response.update(response_params.merge(user_id: current_user.id))
-        format.html { redirect_to question_url, notice: "Response was successfully created." }
+        format.html { redirect_to question_url, notice: "Response was successfully updated." }
         format.turbo_stream { render :update }
         format.json { render :show, status: :ok, location: @response }
       else
